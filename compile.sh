ed2b60fc74d1de06090cd23271507d17d98bd161
archlinuxuser <161124933+0x06K@users.noreply.github.com>
2025-09-08 06:43:12 +0000

    compile.sh

---
+++ compile.sh
@@ -0,0 +1,8 @@
+# Assemble boot.asm
+nasm -f elf32 boot.asm -o boot.o
+
+# Compile C kernel
+gcc -m32 -ffreestanding -c kernel.c -o kernel.o
+
+# Link everything
+ld -m elf_i386 -T linker.ld -o kernel.bin boot.o kernel.o
